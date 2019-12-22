d0ef1e2b2ae7ff906b410f53096fd5221ca6f2e0
Stephen McGuckin <donald.stephen.mcguckin@gmail.com>
2019-12-22 15:35:33 -0800

    adds implementation for SseMiddlewareProvider

--- src/sse-middleware/sse-middleware.provider.ts
+++ src/sse-middleware/sse-middleware.provider.ts
@@ -1,12 +1,23 @@
 import {Context} from 'koa';
 import {SseMiddlewareProviderInterface} from './sse-middleware.interface';
-import {injectable} from 'inversify';
+import {inject, injectable} from 'inversify';
 import 'reflect-metadata';
+import {StreamFactoryInterface} from '../stream/stream.factory.interface';
+import {TYPES} from '../di-container/types';
 
 @injectable()
 export class SseMiddlewareProvider implements SseMiddlewareProviderInterface {
 
-    attachMiddleware(ctx: Context): void {
+    constructor(
+        @inject(TYPES.StreamFactory)
+        private readonly streamFactory: StreamFactoryInterface
+    ) {
+    }
 
+    attachMiddleware(ctx: Context): void {
+        ctx.type = 'text/event-stream';
+        ctx.body = this.streamFactory.getStream();
+        ctx.req.socket.setTimeout(Number.MAX_VALUE);
+        ctx.res.write('\n');
     }
 }
--- src/sse-middleware/sse-middleware.test.ts
+++ src/sse-middleware/sse-middleware.test.ts
@@ -1,5 +1,50 @@
-describe('SseMiddlewareProvider', () => {
-    it('does nothing yet', () => {
+import {SseMiddlewareProvider} from './sse-middleware.provider';
+import {StreamFactoryMock} from '../stream/stream.factory.mock';
+import {StreamFactoryInterface} from '../stream/stream.factory.interface';
+import {Context} from 'koa';
 
+describe('SseMiddlewareProvider', () => {
+
+    let provider: SseMiddlewareProvider;
+    let stream: NodeJS.WritableStream;
+    let streamFactory: StreamFactoryInterface;
+
+    beforeEach(() => {
+        streamFactory = new StreamFactoryMock();
+        streamFactory.getStream = jest.fn().mockImplementation(() => stream);
+        provider = new SseMiddlewareProvider(streamFactory);
     });
+
+    it('sets up context for sse', () => {
+        stream = 'stream' as any;
+
+        const ctx = getFakeContext();
+        provider.attachMiddleware(ctx);
+
+        expect(ctx.type).toEqual('text/event-stream');
+        expect(ctx.body).toBe(stream);
+        expectRequestSocketTimeoutEquals(ctx, Number.MAX_VALUE);
+        expectWrittenResponse(ctx, '\n');
+    });
+
+    function expectRequestSocketTimeoutEquals(ctx: Context, value: number): void {
+        expect(ctx.req.socket.setTimeout).toHaveBeenCalledWith(value);
+    }
+
+    function expectWrittenResponse(ctx: Context, data: any) {
+        expect(ctx.res.write).toHaveBeenCalledWith(data);
+    }
+
+    function getFakeContext(): Context {
+        return {
+            req: {
+                socket: {
+                    setTimeout: jest.fn()
+                }
+            },
+            res: {
+                write: jest.fn()
+            }
+        } as any;
+    }
 });
