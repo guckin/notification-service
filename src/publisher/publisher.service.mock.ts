03c81b4c07f70068ca61e156057daeaa5226d3bc
Stephen McGuckin <donald.stephen.mcguckin@gmail.com>
2019-12-22 16:24:53 -0800

    removes unused method
    forgot to delete. oopsie doopsie!

--- src/publisher/publisher.service.mock.ts
+++ src/publisher/publisher.service.mock.ts
@@ -1,10 +1,6 @@
 import {PublisherServiceInterface} from './publisher.interface';
 
 export class PublisherServiceMock<T> implements PublisherServiceInterface<T> {
-    onPublish(cb: (data: T) => void): void {
-    }
-
     publish(value: T): void {
     }
-
 }
