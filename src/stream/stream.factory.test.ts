7de10700c8e7dbc5338937edd249e2bff09ad948
Stephen McGuckin <donald.stephen.mcguckin@gmail.com>
2019-12-22 17:14:01 -0800

    adds impl for stream factory

--- src/stream/stream.factory.test.ts
+++ src/stream/stream.factory.test.ts
@@ -1,5 +1,17 @@
-describe('StreamFactory', () => {
-    it('does nothing yet', () => {
+import {StreamFactory} from './stream.factory';
+import {PassThrough} from 'stream';
 
+describe('StreamFactory', () => {
+
+    let factory: StreamFactory;
+
+    beforeEach(() => {
+       factory = new StreamFactory() ;
+    });
+
+    it('does nothing yet', () => {
+        const stream = factory.getStream();
+
+        expect(stream).toBeInstanceOf(PassThrough);
     });
 });
--- src/stream/stream.factory.ts
+++ src/stream/stream.factory.ts
@@ -1,10 +1,11 @@
 import {StreamFactoryInterface} from './stream.factory.interface';
 import {injectable} from 'inversify';
 import 'reflect-metadata';
+import {PassThrough} from 'stream';
 
 @injectable()
 export class StreamFactory implements StreamFactoryInterface {
     getStream(): NodeJS.WritableStream {
-        return undefined;
+        return new PassThrough();
     }
 }
